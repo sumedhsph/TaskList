84d2ae147ebe5be668a162e90767346f20a1b66a
sumedhsph <sumedh.sph@gmail.com>
2025-12-01 22:53:52 +0530

    test negative

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,6 +2,6 @@ import { describe, it, expect } from 'vitest'
 
 describe('Todo App Initial Test', () => {
   it('should pass CI successfully', () => {
-    expect(true).toBe(true)
+    expect(true).toBe(false)
   })
 })
